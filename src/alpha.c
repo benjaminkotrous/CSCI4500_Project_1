865ec2768902e36fb0be3e12bbdddc1bca05c593
Benjamin Kotrous <benjaminkotrous@Benjamins-MBP.domain>
2019-09-30 20:32:16 -0500

    latest

--- src/alpha.c
+++ src/alpha.c
@@ -1,7 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hello World\n");
+
+  int status = 0;
+  char c = 'a';
+  int len = 0;
+  char line[101] = {0};
+
+  for(;;){
+  status = read(0,&c,1);
+  //write(1,&c,1);
+  if(status == 0){
+    printf("hello");
+    return(0);
+  }
+
+
+    if(c == '\n'){
+    line[len++] = '\0';
+    break;
+}
+    line[len++] = c;
+  }
+
+
+  line[len++] = '\0';
+  printf(line,'\n');
   return 0;
 }
