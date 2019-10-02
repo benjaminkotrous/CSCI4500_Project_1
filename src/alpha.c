37752451b952b60f1b191e4a782383cd7bf3d39f
benjaminkotrous <bkotrous@unomaha.edu>
2019-10-02 21:39:36 +0000

    ccc

--- src/alpha.c
+++ src/alpha.c
@@ -2,33 +2,54 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-int main(int argc, char const *argv[]) {
+void rd(){
+    int status = 0;
+    char c;
+    int len = 0;
+    int init = 0;
+    int empty = 0;
+    char line[101] = {0};
 
-  int status = 0;
-  char c = 'a';
-  int len = 0;
-  char line[101] = {0};
-
-  for(;;){
-  status = read(0,&c,1);
-  //write(1,&c,1);
-  if(status == 0){
-    printf("hello");
-    return(0);
-  }
+    for(;;){
+        status = read(0,&c,1);
+        //write(1,&c,1);
+        if(status == 0){
+            printf("nothng");
+            break;
+        }
 
 
-    if(c == '\n'){
+
+        if(isalpha(c)){
+            empty = 1;
+            //printf("isChar");
+        }
+            line[len++] = c;
+        if(empty == 0){
+            //printf("If blanks or tabs or nothing entered");
+            continue;
+        }
+        if(c == '\n' && empty == 1){
+            //printf("End Case");
+            break;
+        }
+
+
+
+
+    }
     line[len++] = '\0';
-    break;
+    printf(line,'\n');
 }
-    line[len++] = c;
-  }
 
+void prse(){
 
-  line[len++] = '\0';
-  printf(line,'\n');
-  printf('\n');
+}
+int main(int argc, char const *argv[]) {
+    rd();
+
   return 0;
+
 }
